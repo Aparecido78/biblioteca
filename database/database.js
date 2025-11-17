37fc353c111e970350e9af93e947232245f72350
geraldo aparecido <geraldosanatana447@gmail.com>
2025-11-17 17:41:50 -0300

    arrumando

--- database/database.js
+++ database/database.js
@@ -1,18 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false }
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false }
   }
-);
+});
 
 async function testar() {
   try {
